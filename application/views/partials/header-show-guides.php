6292c9e4f7333465d508b6b7fde771b8a64f788b
Ray Oleaga <yourdesignbuddy13@gmail.com>
2015-08-30 21:09:03 -0700

    guides & map page staying 50 percent done

--- application/views/partials/header-show-guides.php
+++ application/views/partials/header-show-guides.php
@@ -1,20 +1,16 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title></title>
+<title>Show All Guides</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
 <script type="text/javascript" src="/assets/js/maps.js"></script>
-<script type="text/javascript" src="/assets/js/ux-main.js"></script>
-
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<script src="https://maps.googleapis.com/maps/api/js?signed_in=true&callback=initMap" async defer></script>
+<!--******************** CSS ***************** -->
 <!-- fonts -->
 <link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,700' rel='stylesheet' type='text/css'>
-<!-- main css -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
 </head>
-
-
-
--- application/views/show_guides.php
+++ application/views/show_guides.php
@@ -1,54 +1,19 @@
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Show All Guides</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<script src="http://maps.google.com/maps/api/js?sensor=false" 
-type="text/javascript"></script>
-<script type="text/javascript" src="/assets/js/maps.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?signed_in=true&callback=initMap"
-       async defer></script>
-</style>
-<link rel="stylesheet" type="text/css" href="/assets/style.css">
-</head>
+<!-- header -->
+<?php $this->load->view("partials/header-show-guides") ?>
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <span class="navbar-brand"><img src="/assets/navipal_logo.png" alt="Navipal" height="50" width="140"></span>
-    </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-        <li></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="/users">Back</a></li>
-      </ul>
-    </div>
+  <!-- navigation -->
+  <?php $this->load->view("partials/navigation") ?>
+
+
+  <div id="panel" class="bg-color3 margin-top-Negative51 padding-top-60">
+    <input id="address" type="textbox" value="San Jose, CA">
+    <!-- <input id="submit" type="button" value="Geocode"> -->
+    <button type="submit" id="submit" class="explore no-bg" value="Geocode">explore</button>
   </div>
-</nav>
-<div class="main-container">
-  <div class="container">
-    <div id="panel">
-      <input id="address" type="textbox" value="San Jose, CA">
-      <!-- <input id="submit" type="button" value="Geocode"> -->
-      <button type="submit" id="submit" class="btn btn-primary" value="Geocode">Search!</button>
-    </div>
-    <div class="col-md-6">
+  <div class="">
+    <div class="guides-container bg-color2">
       <?php 
       foreach($guides as $guide){
-        echo "<h1><a href='/guides/view_profile/{$guide['id']}'>{$guide['name']}</a></h1>";
-        echo "<img src='/uploads/{$guide['image']}'>";
-        echo "<h4>\"{$guide['description']}\"</h4>";
         echo "<h4>Rating: ";
         for ($i = 0; $i < $guide['rating']; $i++)
         {
@@ -60,16 +25,18 @@ type="text/javascript"></script>
          echo "<img src='/assets/blank.png' height='25' width='25'>";
        }
        echo "</h4>";
+        echo "<img class='profile-photo' src='/uploads/{$guide['image']}'>";
+        echo "<h2><a href='/guides/view_profile/{$guide['id']}'>{$guide['name']}</a></h2>";
+        echo "<h4>\"{$guide['description']}\"</h4>";
        echo "<h4>Price: \${$guide['price']}/night</h4>";
        echo "<h4>{$guide['location']}</h4>";
      }
      ?>
    </div>
-   <div class="col-md-6">
-   <div id="map"><!--do not style this div-->
-   </div>
+   <div class="map-container">
+     <div id="map"><!--do not style this div-->
+     </div>
    </div>
  </div>
-</div><!--/.main-container -->
-</body>
-</html>
+
+<?php $this->load->view("partials/footer"); ?>
