a7dd5b6b7e86da53e4d8a0583e7b39daaae25d37
suesm27 <suesm27@gmail.com>
2015-09-03 11:56:36 -0700

    change latin texts to meaningful content on home page

--- application/views/front-page/index.php
+++ application/views/front-page/index.php
@@ -50,27 +50,27 @@
     <div class="search-container">
       <form  action="/guides/show_guides" method="post" id="search">
        <!--  <input type="search" name="search" placeholder="Search the city..." class="search"> -->
-        <button type="submit" class="btn btn-lg btn-4 explore"> explore here...<span class="glyphicon glyphicon-map-marker"></span></button>
+        <button type="submit" class="btn btn-lg btn-4 explore"> Start Exploring<span class="glyphicon glyphicon-map-marker"></span></button>
       </form>
     </div>
   </div>
   <!-- section 1 -->
   <div class="section-container bg-color1" id="section1">
     <div class="icon-container"><i class="flaticon-magnifier72"></i> </div>
-    <h1>explore</h1>
-    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature.</p>
+    <h1>Explore</h1>
+    <p>Explore a new city through the eyes of a local; the best spots to hangout, the hidden gem restaurants, or a fun way to experience a new place.</p>
   </div>
   <!-- section 2 -->
   <div class="section-container bg-color2" id="section2">
     <div class="icon-container"><i class="flaticon-plussign1"></i></div>
-    <h1>choose a navi<span>pal</span></h1>
-    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature.</p>
+    <h1>Choose a navi<span>pal</span></h1>
+    <p>Meet someone who knows the in’s and out’s of a city. Read their profile and reviews to find your perfect navipal.</p>
   </div>
   <!-- section 3 -->
   <div class="section-container bg-color3" id="section3">
     <div class="icon-container"><i class="flaticon-travel25"></i></div>
-    <h1>happy arrivals</span></h1>
-    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature.</p>
+    <h1>Have fun!</span></h1>
+    <p>Pick your date and start exploring! After your journey, don’t forget to leave a review for your guide!</p>
   </div>
 
   <!-- footer -->
