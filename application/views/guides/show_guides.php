769212f66a1b53c7854950b83ac3ed8aa0a0775b
ray Oleaga <yourdesignbuddy13@gmail.com>
2015-09-03 00:19:54 -0400

    show_guides page 3 column align

--- application/views/guides/show_guides.php
+++ application/views/guides/show_guides.php
@@ -15,12 +15,13 @@
       <?php 
       foreach($guides as $guide){
 ?>
+      <div class="col-md-4">
         <!-- Profile image -->
       <img class='profile-photo' width='125' height='125' src='/uploads/<?php echo "{$guide['id']}.png";?>'>
       <!-- name -->
       <h2 class="guide-name"><a class="text-color4" href='/guides/view_profile/<?php echo $guide['id']?>'><?php echo $guide['name'];?></a></h2>
         <!-- star rating -->
-              <div class="margin-bottom-45">
+              <div class="">
       <?php        
               for ($i = 0; $i < $ratings[$guide['id']]; $i++)
               {
@@ -41,7 +42,8 @@
       <!-- description -->
         <!-- <h4 class="text-color4"><?php echo $guide['description'];?></h4> -->
        <!-- <h4>Price: \<?php echo $guide['price'];?>/night</h4> -->
-       <h4><?php echo $guide['location'];?></h4>
+       <p class="text-color4"><?php echo $guide['location'];?></p class="text-color4">
+   </div>
 <?php 
       }
 ?>
