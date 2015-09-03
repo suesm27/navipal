4ee54c46b4cf57ff23fa692f6a5bd44540b432c1
ray Oleaga <yourdesignbuddy13@gmail.com>
2015-09-03 17:38:27 -0400

    edit profile page styled

--- application/views/guides/edit_guide_profile.php
+++ application/views/guides/edit_guide_profile.php
@@ -1,37 +1,12 @@
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Edit Profile</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
- <style></style>
- <link rel="stylesheet" type="text/css" href="/assets/style.css">
-</head>
-<body>
-  <nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <span class="navbar-brand"></span>
-      </div>
-      <div id="navbar" class="navbar-collapse collapse">
-        <ul class="nav navbar-nav">
-          <li></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="/guides/logoff">Logoff</a></li>
-        </ul>
-      </div><!--/.nav-collapse -->
-    </div><!--/.container -->
-  </nav>
-  <div class="main-container">
+  <!-- header-login -->
+  <?php $this->load->view("partials/header-login"); ?>
+  <body>
+    <!-- navigation -->
+    <?php $this->load->view("partials/navigation"); ?>
+    <div class="bg-color3 margin-top-Negative51 padding-top-60 "></div>
+<div class="bg-color2  padding-top-60 height100">
+  
+
     <div class="container">
       <?php 
       if ($this->session->flashdata('success'))
@@ -65,9 +40,10 @@
       ?>
     </div>
     <div class="container">
+<h2 >Edit Profile</h2>
     <div class="row">
-      <div class="col-md-6">
-        <form class="form-horizontal" roll='form' action="/guides/edit_guide_action/<?php echo $guide['id']; ?>" method='post'>
+      <div class="col-sm-6">
+        <form class="form-horizontal right-margin left-margin" roll='form' action="/guides/edit_guide_action/<?php echo $guide['id']; ?>" method='post'>
           <input type='hidden' name='action' value='basic'>
           <div class="form-group">
             <label>Name: </label>
@@ -94,25 +70,32 @@
             <textarea class="form-control" rows="5" name="description" value="<?php echo $guide['description'];?>" required><?php echo $guide['description'];?></textarea>
           </div> 
           <div class="form-group">
-            <button type="submit" class="btn btn-lg btn-primary">Save</button>
+            <button type="submit" class="btn btn-lg btn-1">Save</button>
           </div>
-        </form>    
-        <form class="form-horizontal" roll='form' action="/guides/edit_guide_action/<?php echo $guide['id']; ?>" method='post'>
-          <input type='hidden' name='action' value='password'>
-          <div class="form-group">
-            <label>Password: </label>
-            <input type="password" class="form-control" name="password" required>
-          </div>
-          <div class="form-group">
-            <label>Password Confirmation: </label>
-            <input type="password" class="form-control" name="passwordconf" required>
-          </div>
-          <div class="form-group">
-            <button type="submit" class="btn btn-lg btn-primary">Update Password</button>
-          </div>
-        </form>
+        </form> 
+        </div>
+        <div class="col-sm-6">
+          <form class="form-horizontal right-margin left-margin" roll='form' action="/guides/edit_guide_action/<?php echo $guide['id']; ?>" method='post'>
+            <input type='hidden' name='action' value='password'>
+            <div class="form-group">
+              <label>Password: </label>
+              <input type="password" class="form-control" name="password" required>
+            </div>
+            <div class="form-group">
+              <label>Password Confirmation: </label>
+              <input type="password" class="form-control" name="passwordconf" required>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-lg btn-1">Update Password</button>
+            </div>
+          </form>
+
+        </div>
     </div>
-  </div> <!-- /container -->
+  
+</div>  
 </div>
-</body>
-</html>
+ <!-- footer -->
+    <?php
+    $this->load->view("partials/footer");
+    ?>
